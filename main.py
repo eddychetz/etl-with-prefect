a025ea778a338897918d040b2161bef1a4a37817
Eddwin Cheteni <39814037+eddychetz@users.noreply.github.com>
2026-02-01 23:03:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 @task
 def step_1():
     print('Hello, step 1 is running!!')
-@flowdef main()
-:step_message = step1()print(step_message)   
+@flow
+def main()
+    :step_message = step1()print(step_message)   
