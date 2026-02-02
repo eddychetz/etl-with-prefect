3e9c6a10cc0d087b3794d7739d0d1b67698ca4c4
Eddwin Cheteni <39814037+eddychetz@users.noreply.github.com>
2026-02-02 08:25:33 +0200

    add

--- main.py
+++ main.py
@@ -1,6 +1,46 @@
+# Install Prefect ----
+# * pip install prefect
+# * prefect cloud login and choose to login with a web browser
+
+# * run `prefect deploy etl-with-prefect/main.py:main_flow`
+# * run `prefect dashboard`
+
+
+
+# 1.0 Libraries ----
+from prefect import flow, task#, get_run_logger
+
+# 2.0 Extract ----
 @task
-def step_1():
+def extract():
     print('Hello, step 1 is running!!')
-@flow
-def main()
-    :step_message = step1()print(step_message)   
+
+# 3.0 Transform ----
+@task
+def transform():
+    print('Hello, step 2 is running!!')
+
+# 4.0 Load ----
+@task
+def load():
+    print('Hello, step 3 is running!!')
+
+# 5.0 Main Flow ----
+@flow(name='ETL Workflow')
+def main_flow():
+    step1 = extract()
+    print(step1)
+    step2 = transform()
+    print(step2)
+    step3 = load()
+    print(step3)
+
+# 6.0 Run ----
+if __name__ == '__main__':
+    main_flow()
+
+
+# 7.0 Deployment
+# * run `prefect api`
+# * run `prefect deploy etl-with-prefect/main.py:main_flow -name test-deployment`
+# * run `prefect dashboard`
